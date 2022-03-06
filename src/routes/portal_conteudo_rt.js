fbc8d4fbb649b7a79ec58c61862a2cf2a0830492
RobertoLPereira <roberto.lpereira65@gmail.com>
2022-03-06 15:00:59 -0300

    Update portal_conteudo_rt.js
    Atualização da rota

--- src/routes/portal_conteudo_rt.js
+++ src/routes/portal_conteudo_rt.js
@@ -2,6 +2,6 @@ module.exports = app => {
     const Pessoas = app.db.models.pessoas;
     app.route('/')
     .get((req, res) => {
-      res.render('../views/Portal_Conteudo');
+      res.render('../src/views/Portal_Conteudo.ejs');
     });
   };
