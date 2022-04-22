edd250d11d58ff38019df379032508c22c273301
RobertoLPereira <roberto.lpereira65@gmail.com>
2022-04-22 15:53:51 -0300

    leitura consumo de agua
    Manutenção corretiva na rotina de salvar leitura de consumo de água.

--- routes/leituraaguaunidade_rt.js
+++ routes/leituraaguaunidade_rt.js
@@ -4,6 +4,7 @@
 	 module.exports = app => {
     const Leituraaguaunidade = app.db.models.leituraaguaunidade;
     const Faixadeconsumo = app.db.models.faixadeconsumo;
+    var data = new Date();
 
     app.route('/Leituraaguaunidade')
       .get((req, res) => {
@@ -26,19 +27,40 @@
             status: req.body.status,
             valor: req.body.valor,
         }
+        if (leituraaguaunidade.dataleitura == 'null' || leituraaguaunidade.dataleitura == null){
+          var dia = String(data.getDate()).padStart(2, '0');
+          var mes = String(data.getMonth() + 1).padStart(2, '0');
+          var ano = data.getFullYear();
+          if (dia.length < 2) dia = "0" + dia;
+          if (mes.length < 2) mes = "0" + mes;
+          if (ano.length < 4) ano = "19" + ano;
+          leituraaguaunidade.dataleitura = dia + '/' + mes + '/' + ano;
+        }
         Faixadeconsumo.sequelize.query(calculo)
-        .then(result => {
-            var valor =result[0];
-            leituraaguaunidade.valor = valor[0].valor;
-            Leituraaguaunidade.create(leituraaguaunidade)
-            .then(result => res.json(result))
-            ;
+          .then(result => {
+              var valor =result[0];
+              leituraaguaunidade.valor = valor[0].valor;
+              Leituraaguaunidade.create(leituraaguaunidade)
+                .then(result => res.json(result))
+                .catch(error => {
+                   res.status(412).json({msg: error.message});
+                 });
           })
-        .catch(error => {
-              res.status(412).json({msg: error.message});
-        });
+          .catch(error => {
+            res.status(412).json({msg: error.message});
+          });        
       })
       .put((req, res) => {
+        var dataatua;
+        if (req.body.dataleitura == 'null' || req.body.dataleitura == null){
+          var dia = String(data.getDate()).padStart(2, '0');
+          var mes = String(data.getMonth() + 1).padStart(2, '0');
+          var ano = data.getFullYear();
+          if (dia.length < 2) dia = "0" + dia;
+          if (mes.length < 2) mes = "0" + mes;
+          if (ano.length < 4) ano = "19" + ano;
+          dataatua = dia + '/' + mes + '/' + ano;
+        }
         var vlr = (req.body.leituraatual - req.body.leituraanterior);       
         var calculo = "select (("+vlr+"*tarifaagua)+(tarifaesgoto*2)*(tfdi)) as valor from faixadeconsumo where "+vlr+" between faixainicial and faixafinal";              
         Faixadeconsumo.sequelize.query(calculo)
@@ -46,7 +68,7 @@
             var valor =result[0];
              var leituraaguaunidade = "update leituraaguaunidade set  idunidadeimovel="+ req.body.idunidadeimovel+
              ",leituraanterior="+ req.body.leituraanterior+",leituraatual="+ req.body.leituraatual+
-             ",codigomedidor='"+ req.body.codigomedidor+"',dataleitura='"+ req.body.dataleitura+"', status="+
+             ",codigomedidor='"+ req.body.codigomedidor+"',dataleitura='"+ dataatua+"', status="+
              req.body.status+"valor="+valor+" where  idleituraaguaunidade=" +req.body.idleituraaguaunidade;            
             Leituraaguaunidade.sequelize.query(leituraaguaunidade)
             .then(result =>  res.sendStatus(204))
@@ -59,7 +81,6 @@
             });
       });
           
-
     app.route('/Leituraaguaunidade/:idleituraaguaunidade')
       .get((req, res) => {
         var idleituraaguaunidade = parseInt(req.params.idleituraaguaunidade);
